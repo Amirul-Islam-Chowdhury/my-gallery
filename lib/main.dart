8479f7c0798c277602719f67daec8403f6f9ab5e
Amirul-Islam-Chowdhury <aislamchy96@gmail.com>
2021-12-15 22:16:24 +0200

    add-firebase-auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:my_gallery/screens/login_screen.dart';
+import 'package:my_gallery/screens/loginScreen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
